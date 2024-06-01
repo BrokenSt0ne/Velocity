ee47e9a89bdcb4de60dcc961e32d2e1337f9e3c6
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-06-01 15:05:31 -0400

    0.4.4

--- Assets/Scripts/UI/SplashText.cs
+++ Assets/Scripts/UI/SplashText.cs
@@ -26,7 +26,15 @@ namespace UI
             "Guaranteed!",
             "This year is the year of the Linux desktop, I promise",
             "Made by gay cats",
-            "It's called Twitter, not X"
+            "It's called Twitter, not X",
+            "Object reference not set to an instance of an object",
+            "If it works, don't fix it",
+            "Game design is my passion",
+            "Try Half-Life!",
+            "Go fast!",
+            "It's free!",
+            "Trans rights",
+            "Open-source!"
         };
 
         private void Awake() {
